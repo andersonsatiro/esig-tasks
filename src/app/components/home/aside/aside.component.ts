7ced76bdc95dee5e6a0401279af0f92cd016ff76
AndersonPaivaS <andersonpaiva135@gmail.com>
2024-08-02 16:44:02 -0300

    feat: create aside from homepage

--- src/app/components/home/aside/aside.component.ts
+++ src/app/components/home/aside/aside.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-aside',
   standalone: true,
-  imports: [],
+  imports: [
+  ],
   templateUrl: './aside.component.html',
   styleUrl: './aside.component.scss'
 })
