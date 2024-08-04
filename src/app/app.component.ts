95360ca5cebe649523ae01d31438187df0a19db4
AndersonPaivaS <andersonpaiva135@gmail.com>
2024-08-04 20:04:15 -0300

    feat: creating a to-do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AsideComponent } from './components/home/aside/aside.component';
 import { HeaderComponent } from './components/home/header/header.component';
@@ -6,6 +6,17 @@ import { MainFixedContentComponent } from './components/home/main-fixed-content/
 import { DashboardComponent } from './components/home/dashboard/dashboard.component';
 import { ToDoListComponent } from './components/home/to-do-list/to-do-list.component';
 import { CommonModule } from '@angular/common';
+
+type taskPriority = 'alta' | 'média' | 'baixa'
+type taskStatus = 'não iniciada' | 'em andamento' | 'para hoje' | 'entregue'
+
+interface Task {
+  task: string,
+  priority: taskPriority,
+  responsible: string,
+  status: taskStatus,
+  end_date: string,
+}
 
 @Component({
   selector: 'app-root',
@@ -23,10 +34,46 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  @Input() tasks: Task[] = [
+    {
+      task: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      priority: 'alta',
+      responsible: 'José Augusto',
+      status: 'entregue',
+      end_date: 'Seg, 11 jun, 2024',
+    },
+    {
+      task: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      priority: 'média',
+      responsible: 'Maria Vicente',
+      status: 'em andamento',
+      end_date: 'Ter, 39 ago, 2024',
+    },
+    {
+      task: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      priority: 'baixa',
+      responsible: 'Ricardo Moreira',
+      status: 'não iniciada',
+      end_date: 'Qua, 05 ago, 2024',
+    },
+    {
+      task: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      priority: 'alta',
+      responsible: 'Théo Henrique',
+      status: 'para hoje',
+      end_date: 'Qui, 08 set, 2024',
+    },
+    {
+      task: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      priority: 'baixa',
+      responsible: 'Noberto Silva',
+      status: 'entregue',
+      end_date: 'Sex, 23 out, 2024',
+    }
+  ];
   title = 'esig-tasks';
-
-  currentView: string = 'dashboard';
-
+  currentView: string = 'tasks';
+  
   setView(view: string) {
     this.currentView = view;
   }
--- src/app/components/home/to-do-list/to-do-list.component.ts
+++ src/app/components/home/to-do-list/to-do-list.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { InputSelectComponent } from '../input-select/input-select.component';
+import { CommonModule } from '@angular/common';
+
+type taskPriority = 'alta' | 'média' | 'baixa'
+type taskStatus = 'não iniciada' | 'em andamento' | 'para hoje' | 'entregue'
+
+interface Task {
+  task: string,
+  priority: taskPriority,
+  responsible: string,
+  status: taskStatus,
+  end_date: string,
+}
 
 @Component({
   selector: 'app-to-do-list',
   standalone: true,
-  imports: [],
+  imports: [
+    InputSelectComponent,
+    CommonModule
+  ],
   templateUrl: './to-do-list.component.html',
   styleUrl: './to-do-list.component.scss'
 })
 export class ToDoListComponent {
+  @Input() tasks: Task[] = [];
 
+  getPriorityColor(priority: string, local: string){
+    switch (priority) {
+      case 'alta':
+        return local === 'bg' ? '#FFDADA' : '#FF1414'    
+      case 'média':
+        return local === 'bg' ? '#FFF2DD' : '#D98634'
+      default:
+        return local === 'bg' ? '#AEE5FD' : '#00ADF8' 
+    }
+  }
+
+  getStatusColor(status: string, local: string){
+    switch (status) {
+      case 'não iniciada':
+        return local === 'bg' ? '#E4E4E4' : '#3F3748'
+      case 'em andamento':
+        return local === 'bg' ? '#AEE5FD' : '#00ADF8'
+      case 'para hoje':
+        return local === 'bg' ? '#FFF2DD' : '#D98634'
+      default:
+        return local === 'bg' ? '#CCFDD9' : '#409261'
+    }
+  }
 }
